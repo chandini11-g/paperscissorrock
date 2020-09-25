4aae5c1d9a142259a3fd3d7613849f5f83197fda
gitpractice <gitpractice@gmail.com>
2020-09-25 21:23:02 +0530

    made changes in index1

--- index1.js
+++ index1.js
@@ -25,52 +25,49 @@
 let userchoice = window.prompt("select any one 1.paper,2.scissor or 3.rock ");
 document.write("<h4>users choice is</h4>");
 let userimage = '<img src="' + userchoice + '.jpg">';
-document.write(userimage);
-document.write("<h4>computer choice is</h4>");
-let computerimage = '<img src="' + Math.floor(1 + Math.random() * 3) + '.jpg">';
-document.write(computerimage);
-document.write("<p>results:</p>");
-if (userimage === computerimage) document.write("<p>it's tie.</p>");
-else if (
-  userimage === '<img src="' + 3 + '.jpg">' &&
-  computerimage === '<img src="' + 2 + '.jpg">'
-) {
-  document.write(" <p>user wins!rock smashes scissor</p>");
-} else if (
-  userimage === '<img src="' + 1 + '.jpg">' &&
-  computerimage === '<img src="' + 2 + '.jpg">'
-) {
-  document.write("<p>computer wins! scissor cuts paper</p>");
-} else if (
-  userimage === '<img src="' + 2 + '.jpg">' &&
-  computerimage === '<img src="' + 1 + '.jpg">'
-) {
-  document.write("<p>user wins! scissor cuts paper</p>");
-} else if (
-  userimage === '<img src="' + 2 + '.jpg">' &&
-  computerimage === '<img src="' + 3 + '.jpg">'
-) {
-  document.write(" <p>computer wins!rock smashes scissor</P>");
-} else if (
-  userimage === '<img src="' + 3 + '.jpg">' &&
-  computerimage === '<img src="' + 1 + '.jpg">'
-) {
-  document.write("<p> computer wins!paper covers rock</p>");
-} else if (
-  userimage === '<img src="' + 1 + '.jpg">' &&
-  computerimage === '<img src="' + 3 + '.jpg">'
-) {
-  document.write("<p> user wins!paper covers rock</p>");
-} else if (
-  userimage !== '<img src="' + 3 + '.jpg">' &&
+if (
+  userimage !== '<img src="' + 1 + '.jpg">' &&
   userimage !== '<img src="' + 2 + '.jpg">' &&
-  userimage !== '<img src="' + 1 + '.jpg">'
+  userimage !== '<img src="' + 3 + '.jpg">'
 ) {
-  document.write(
-    "computer choice is :" +
-      computchoice +
-      " users choice is : " +
-      userchoice +
-      " Invalid choice! Choose only numbers 1,2 and 3"
-  );
+  document.write("inavalid enter only numbers 1,2,3");
+} else {
+  document.write(userimage);
+  document.write("<h4>computer choice is</h4>");
+  let computerimage =
+    '<img src="' + Math.floor(1 + Math.random() * 3) + '.jpg">';
+  document.write(computerimage);
+  document.write("<p>results:</p>");
+  if (userimage === computerimage) document.write("<p>it's tie.</p>");
+  else if (
+    userimage === '<img src="' + 3 + '.jpg">' &&
+    computerimage === '<img src="' + 2 + '.jpg">'
+  ) {
+    document.write(" <p>user wins!rock smashes scissor</p>");
+  } else if (
+    userimage === '<img src="' + 1 + '.jpg">' &&
+    computerimage === '<img src="' + 2 + '.jpg">'
+  ) {
+    document.write("<p>computer wins! scissor cuts paper</p>");
+  } else if (
+    userimage === '<img src="' + 2 + '.jpg">' &&
+    computerimage === '<img src="' + 1 + '.jpg">'
+  ) {
+    document.write("<p>user wins! scissor cuts paper</p>");
+  } else if (
+    userimage === '<img src="' + 2 + '.jpg">' &&
+    computerimage === '<img src="' + 3 + '.jpg">'
+  ) {
+    document.write(" <p>computer wins!rock smashes scissor</P>");
+  } else if (
+    userimage === '<img src="' + 3 + '.jpg">' &&
+    computerimage === '<img src="' + 1 + '.jpg">'
+  ) {
+    document.write("<p> computer wins!paper covers rock</p>");
+  } else if (
+    userimage === '<img src="' + 1 + '.jpg">' &&
+    computerimage === '<img src="' + 3 + '.jpg">'
+  ) {
+    document.write("<p> user wins!paper covers rock</p>");
+  }
 }
